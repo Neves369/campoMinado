583a7e37ba3a04b327c198d04ac092cd2478a4c4
Neves369 <douglasbrian369@hotmail.com>
2024-01-03 15:59:13 -0300

    elementos do jogo

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import params from './src/params';
+import Field from './src/components/Field';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.welcome}>Iniciando o Mines!</Text>
+      <Text 
+        style={styles.instructions}
+      >
+        Tamanho da grade: 
+        {params.getRowsAmount()}x{params.getColumnsAmount()}
+      </Text>
+      <Field/>
+      <Field opened/>
+      <Field opened nearMines={1}/>
+      <Field mined/>
+      <Field mined opened/>
+      <Field mined opened exploded/>
+      <Field flagged/>
     </View>
   );
 }
@@ -17,4 +31,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center"
+  },
+  instructions: {
+    fontSize: 20,
+    textAlign: "center"
+  }
 });
---
+++ src/components/Field.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import Mine from './Mine';
+import Flag from './Flag';
+import params from '../params';
+import { StyleSheet, View, Text } from 'react-native';
+
+export default props => {
+
+    const {mined, opened, nearMines, exploded, flagged} = props;
+
+    const styleField = [styles.field]
+    if(opened) styleField.push(styles.opened)
+    if(exploded) styleField.push(styles.exploded)
+    if(flagged) styleField.push(styles.flagged)
+    if(!opened && !exploded) styleField.push(styles.regular)
+
+    let color = null
+    if(nearMines > 0){
+        if(nearMines == 1) color = "#2a28d7"
+        if(nearMines == 2) color = "#2b520f"
+        if(nearMines > 2 && nearMines < 6) color = "#fff202b0"
+        if(nearMines >= 6) color = "#f30b0b"
+    }
+
+    return(
+        <View style={styleField}>
+            {
+                !mined && opened && nearMines > 0 ?
+                    <Text style={[styles.label, {color: color}]}>{nearMines}</Text>
+                :
+                    false
+            }
+            {mined && opened ? <Mine/> : false}
+            {flagged && !opened? <Flag/> : false}
+        </View>
+    );
+}
+
+const styles= StyleSheet.create({
+    field: {
+        height: params.blockSize,
+        width: params.blockSize,
+        borderWidth: params.borderSize,
+    },
+    regular: {
+        backgroundColor: "#999",
+        borderLeftColor: "#ccc",
+        borderTopColor: "#ccc",
+        borderRightColor: "#333",
+        borderBottomColor: "#333",
+    },
+    opened: {
+        backgroundColor: "#b4b4b4",
+        borderColor: "#969696",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    label: {
+        fontWeight: "bold",
+        fontSize: params.fontSize,
+    },
+    exploded: {
+        backgroundColor: "red",
+        borderColor: "red",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+})
